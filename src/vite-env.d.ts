0a5a540f2c4846fb8562d4cd51d71039e7ffe5ef
anon-haf <anonhyo.haf@gmail.com>
2026-07-08 00:27:07 +0300

    this should work

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,2 @@
 /// <reference types="vite/client" />
-<<<<<<< HEAD
-=======
-
-declare module '*.css';
->>>>>>> 3f4be276 (Clean repo history and publish current app state)
+declare module '*.css';3f4be276 (Clean repo history and publish current app state)
